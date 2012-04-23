58f1e0d8dc95210c1fa7e22f62cfcc0f6e30bdd6
axilmar <axilmar@gmail.com>
2012-04-23 11:43:00 +0300

    version 0.0.0.1.

--- source/parserlib.hpp
+++ source/parserlib.hpp
@@ -2,22 +2,7 @@
 #define PARSERLIB_HPP
 
 
-#include "rule.hpp"
-
-
-namespace parserlib {
-
-
-/** parses an input.
-    @param input input to parse.
-    @param grammar root rule of the grammar.
-    @param ws whitespace rule.
-    @return true on success, false on failure.
- */
-bool parse(input_buffer &input, rule &grammar, rule &ws); 
-
-
-} //namespace parserlib
+#include "ast.hpp"
 
 
 #endif //PARSERLIB_HPP
