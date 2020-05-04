00938bbb565ae951539b7ababf3410cd941f0897
Margaritis, Achillefs (I/EE-331, extern) <extern.achillefs.margaritis@elektrobit.com>
2020-05-04 15:04:13 +0300

    Work in progress.

--- include/parserlib/Rule.hpp
+++ include/parserlib/Rule.hpp
@@ -6,47 +6,11 @@
 #include "Expression.hpp"
 #include "UnaryOperatorsBase.hpp"
 #include "ParseContext.hpp"
+#include "RuleReference.hpp"
 
 
 namespace parserlib
 {
-
-
-    template <typename ParseContextType> class Rule;
-
-
-    /**
-        Wrapper class around a rule reference.
-        @param ParseContextType parse context type.
-     */
-    template <typename ParseContextType> class RuleReference :
-        public Expression,
-        public UnaryOperatorsBase<RuleReference<ParseContextType>>
-    {
-    public:
-        /**
-            Constructor.
-            @param rule rule reference.
-         */
-        RuleReference(Rule<ParseContextType>& rule) :
-            m_rule(rule)
-        {
-        }
-
-        /**
-            The parse function.
-            @param pc parse context.
-            @return true on success, false on failure.
-         */
-        bool parse(ParseContextType& pc) const
-        {
-            return m_rule.parse(pc);
-        }
-
-    private:
-        //rule reference.
-        Rule<ParseContextType>& m_rule;
-    };
 
 
     /**
---
+++ include/parserlib/RuleReference.hpp
@@ -0,0 +1,49 @@
+#ifndef PARSERLIB_RULEREFERENCE_HPP
+#define PARSERLIB_RULEREFERENCE_HPP
+
+
+namespace parserlib
+{
+
+
+    template <typename ParseContextType> class Rule;
+
+
+    /**
+        Wrapper class around a rule reference.
+        @param ParseContextType parse context type.
+     */
+    template <typename ParseContextType> class RuleReference :
+        public Expression,
+        public UnaryOperatorsBase<RuleReference<ParseContextType>>
+    {
+    public:
+        /**
+            Constructor.
+            @param rule rule reference.
+         */
+        RuleReference(Rule<ParseContextType>& rule) :
+            m_rule(rule)
+        {
+        }
+
+        /**
+            The parse function.
+            @param pc parse context.
+            @return true on success, false on failure.
+         */
+        bool parse(ParseContextType& pc) const
+        {
+            return m_rule.parse(pc);
+        }
+
+    private:
+        //rule reference.
+        Rule<ParseContextType>& m_rule;
+    };
+
+
+} //namespace parserlib
+
+
+#endif //PARSERLIB_RULEREFERENCE_HPP
