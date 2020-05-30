7067a37a757acdc00462c0aa94ad99a643c54dd4
axilmar <axilmar@gmail.com>
2020-05-30 09:38:50 +0300

    Rewrite :-)

--- include/parserlib.hpp
+++ include/parserlib.hpp
@@ -2,22 +2,4 @@
 #define PARSERLIB_HPP
 
 
-#include "parserlib/AST.hpp"
-#include "parserlib/Choice.hpp"
-#include "parserlib/Expression.hpp"
-#include "parserlib/MatchExpression.hpp"
-#include "parserlib/LogicalAnd.hpp"
-#include "parserlib/LogicalNot.hpp"
-#include "parserlib/OneOrMore.hpp"
-#include "parserlib/Optional.hpp"
-#include "parserlib/ParseContext.hpp"
-#include "parserlib/Range.hpp"
-#include "parserlib/Rule.hpp"
-#include "parserlib/Sequence.hpp"
-#include "parserlib/Set.hpp"
-#include "parserlib/Terminal.hpp"
-#include "parserlib/TerminalSequence.hpp"
-#include "parserlib/ZeroOrMore.hpp"
-
-
 #endif //PARSERLIB_HPP
