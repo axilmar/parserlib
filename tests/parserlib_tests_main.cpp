b4cb4dc2c605d33aa460dd60ff3775442cf01c97
Margaritis, Achillefs (I/EE-331, extern) <extern.achillefs.margaritis@elektrobit.com>
2020-06-15 14:08:48 +0300

    While it would be cool to parse Java with parserlib, the task is too big for me currently :-).

--- tests/parserlib_tests_main.cpp
+++ tests/parserlib_tests_main.cpp
@@ -3,14 +3,12 @@
 
 extern void run_calculator_tests();
 extern void run_xml_tests();
-extern void run_java_tests();
 
 
 int main(int argc, char* argv[])
 {
-    //run_calculator_tests();
-    //run_xml_tests();
-    run_java_tests();
+    run_calculator_tests();
+    run_xml_tests();
     system("pause");
     return 0;
 }
