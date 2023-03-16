234b725d953c1415bcd11e317006001e69449ab9
axilmar <axilmar@gmail.com>
2023-03-16 16:47:13 +0200

    Added differentiation between debugging an inline expression and a reference to an expression.

--- include/parserlib/debug.hpp
+++ include/parserlib/debug.hpp
@@ -9,20 +9,19 @@ namespace parserlib
 {
 
 
-    /**
-        
-        A helper parser for debugging.
+    /**        
+        A helper parser for debugging an inline expression.
         @param T type of expression to debug.
      */
     template <typename T> 
-    class debug : public expression
+    class debug_val : public expression
     {
     public:
         /**
             Constructor.
             @param expression expression.
          */
-        debug(const T& expression)
+        debug_val(const T& expression)
             : m_expression(expression)
         {
         }
@@ -42,6 +41,56 @@ namespace parserlib
     private:
         T m_expression;
     };
+
+
+    /**
+        A helper parser for debugging an expression passed by reference.
+        @param T type of expression to debug.
+     */
+    template <typename T>
+    class debug_ref : public expression
+    {
+    public:
+        /**
+            Constructor.
+            @param expression expression.
+         */
+        debug_ref(T& expression)
+            : m_expression(expression)
+        {
+        }
+
+        /**
+            Parses the given expression.
+            @param pc parse context.
+            @return always 'accepted'.
+         */
+        template <typename ParseContext>
+        parse_result parse(ParseContext& pc) const
+        {
+            parse_result result = m_expression.parse(pc);
+            return result;
+        }
+
+    private:
+        T& m_expression;
+    };
+
+
+    /**
+     * Debugs an inline expression.
+     */
+    template <class T> debug_val<T> debug(const T& expr) {
+        return {expr};
+    }
+
+
+    /**
+     * Debugs an expression by reference.
+     */
+    template <class T> debug_ref<T> debug(T& expr) {
+        return { expr };
+    }
 
 
 } //namespace parserlib
