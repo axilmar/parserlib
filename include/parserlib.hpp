000bec84ce90d91cba93aae26c3596e191983579
axilmar <axilmar@gmail.com>
2023-04-25 14:46:54 +0300

    Added the empty parser symbol.

--- include/parserlib.hpp
+++ include/parserlib.hpp
@@ -7,6 +7,7 @@
 #include "parserlib/TerminalRangeParser.hpp"
 #include "parserlib/TerminalSetParser.hpp"
 #include "parserlib/EOFParser.hpp"
+#include "parserlib/EmptyParser.hpp"
 #include "parserlib/Rule.hpp"
 #include "parserlib/util.hpp"
 
