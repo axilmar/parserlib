d56b340c776c505e002d8bfe7f12359d5a306725
axilmar <axilmar@gmail.com>
2025-02-18 19:09:04 +0200

    Removed redundant header.

--- include/parserlib/parser.hpp
+++ include/parserlib/parser.hpp
@@ -12,6 +12,13 @@
 namespace parserlib {
 
 
+    template <class Parser> class zero_or_more_parser;
+    template <class Parser> class one_or_more_parser;
+    template <class Parser> class optional_parser;
+    template <class Parser> class logical_and_parser;
+    template <class Parser> class logical_not_parser;
+
+
     template <class Derived>
     class parser {
     public:
@@ -27,6 +34,36 @@ namespace parserlib {
 
         auto operator !() const noexcept;
     };
+
+
+    template <class Derived>
+    auto parser<Derived>::operator *() const noexcept {
+        return zero_or_more_parser(get_parser_wrapper(*this));
+    }
+
+
+    template <class Derived>
+    auto parser<Derived>::operator +() const noexcept {
+        return one_or_more_parser(get_parser_wrapper(*this));
+    }
+
+
+    template <class Derived>
+    auto parser<Derived>::operator -() const noexcept {
+        return optional_parser(get_parser_wrapper(*this));
+    }
+
+
+    template <class Derived>
+    auto parser<Derived>::operator &() const noexcept {
+        return logical_and_parser(get_parser_wrapper(*this));
+    }
+
+
+    template <class Derived>
+    auto parser<Derived>::operator !() const noexcept {
+        return logical_not_parser(get_parser_wrapper(*this));
+    }
 
 
 } //namespace parserlib
