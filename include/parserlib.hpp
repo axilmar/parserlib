e3cb9c2413c2acd68b9ac639bc99863846c4945e
axilmar <axilmar@gmail.com>
2025-03-17 13:18:27 +0200

    Placed all the parser classes in their own common header.

--- include/parserlib.hpp
+++ include/parserlib.hpp
@@ -2,29 +2,7 @@
 #define PARSERLIB_HPP
 
 
-#include "parserlib/ast_node.hpp"
-#include "parserlib/case_insensitive_comparator.hpp"
-#include "parserlib/choice_parser.hpp"
-#include "parserlib/end_parser.hpp"
-#include "parserlib/error_parser.hpp"
-#include "parserlib/function_parser.hpp"
-#include "parserlib/logical_and_parser.hpp"
-#include "parserlib/logical_not_parser.hpp"
-#include "parserlib/match_parser.hpp"
-#include "parserlib/newline_parser.hpp"
-#include "parserlib/one_or_more_parser.hpp"
-#include "parserlib/optional_parser.hpp"
-#include "parserlib/parse_context.hpp"
-#include "parserlib/rule.hpp"
-#include "parserlib/sequence_parser.hpp"
-#include "parserlib/stream_wrapper.hpp"
-#include "parserlib/string_wrapper.hpp"
-#include "parserlib/terminal_parser.hpp"
-#include "parserlib/terminal_range_parser.hpp"
-#include "parserlib/terminal_set_parser.hpp"
-#include "parserlib/terminal_string_parser.hpp"
-#include "parserlib/utf8_string.hpp"
-#include "parserlib/zero_or_more_parser.hpp"
+#include "parserlib/parsers.hpp"
 
 
 #endif //PARSERLIB_HPP
---
+++ include/parserlib/parsers.hpp
@@ -0,0 +1,31 @@
+#ifndef PARSERLIB_PARSERS_HPP
+#define PARSERLIB_PARSERS_HPP
+
+
+#include "parserlib/ast_node.hpp"
+#include "parserlib/case_insensitive_comparator.hpp"
+#include "parserlib/choice_parser.hpp"
+#include "parserlib/end_parser.hpp"
+#include "parserlib/error_parser.hpp"
+#include "parserlib/function_parser.hpp"
+#include "parserlib/logical_and_parser.hpp"
+#include "parserlib/logical_not_parser.hpp"
+#include "parserlib/match_parser.hpp"
+#include "parserlib/newline_parser.hpp"
+#include "parserlib/one_or_more_parser.hpp"
+#include "parserlib/optional_parser.hpp"
+#include "parserlib/parse_context.hpp"
+#include "parserlib/rule.hpp"
+#include "parserlib/sequence_parser.hpp"
+#include "parserlib/stream_wrapper.hpp"
+#include "parserlib/string_wrapper.hpp"
+#include "parserlib/terminal_any_parser.hpp"
+#include "parserlib/terminal_parser.hpp"
+#include "parserlib/terminal_range_parser.hpp"
+#include "parserlib/terminal_set_parser.hpp"
+#include "parserlib/terminal_string_parser.hpp"
+#include "parserlib/utf8_string.hpp"
+#include "parserlib/zero_or_more_parser.hpp"
+
+
+#endif //PARSERLIB_PARSERS_HPP
