41f5e4106c1f8343d380b3bb24bb84660eebf74a
axilmar <axilmar@gmail.com>
2025-10-19 13:27:44 +0300

    Rewrite :-)

--- project/main.cpp
+++ project/main.cpp
@@ -1,30 +1,3 @@
-extern void test_parse_context();
-extern void test_terminal_parse_nodes();
-extern void test_unary_parse_nodes();
-extern void test_binary_parse_nodes();
-extern void test_other_parse_nodes();
-extern void test_rule();
-extern void test_utility();
-extern void test_strings();
-extern void test_error_handling();
-extern void test_lexer_parser();
-
-
-static void run_tests() {
-    test_parse_context();
-    test_terminal_parse_nodes();
-    test_unary_parse_nodes();
-    test_binary_parse_nodes();
-    test_other_parse_nodes();
-    test_rule();
-    test_utility();
-    test_strings();
-    test_error_handling();
-    test_lexer_parser();
-}
-
-
 int main() {
-    run_tests();
     return 0;
 }
