2a07e01f63045c82b52b89a6fa2ccb7116bb9215
axilmar <axilmar@gmail.com>
2025-12-20 14:57:17 +0200

    Preparing the dev branch.

--- include/parserlib.hpp
+++ include/parserlib.hpp
@@ -2,31 +2,4 @@
 #define PARSERLIB_HPP
 
 
-#include "parserlib/default_source_position.hpp"
-#include "parserlib/text_source_position.hpp"
-#include "parserlib/default_symbol_comparator.hpp"
-#include "parserlib/case_sensitive_symbol_comparator.hpp"
-#include "parserlib/container_parse_context.hpp"
-#include "parserlib/istream_parse_context.hpp"
-#include "parserlib/symbol_parse_node.hpp"
-#include "parserlib/string_parse_node.hpp"
-#include "parserlib/symbol_set_parse_node.hpp"
-#include "parserlib/symbol_range_parse_node.hpp"
-#include "parserlib/any_parse_node.hpp"
-#include "parserlib/end_parse_node.hpp"
-#include "parserlib/bool_parse_node.hpp"
-#include "parserlib/debug_parse_node.hpp"
-#include "parserlib/newline_parse_node.hpp"
-#include "parserlib/error_parse_node.hpp"
-#include "parserlib/loop_parse_node.hpp"
-#include "parserlib/optional_parse_node.hpp"
-#include "parserlib/logical_and_parse_node.hpp"
-#include "parserlib/logical_not_parse_node.hpp"
-#include "parserlib/sequence_parse_node.hpp"
-#include "parserlib/choice_parse_node.hpp"
-#include "parserlib/match_parse_node.hpp"
-#include "parserlib/parse_node_ptr.hpp"
-#include "parserlib/rule.hpp"
-
-
 #endif // PARSERLIB_HPP
