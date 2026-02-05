cfd972b5309ed67d69cb669514f8c8aefd47ed3a
axilmar <axilmar@gmail.com>
2026-02-05 12:41:54 +0200

    Fixed loop 0 parsing to stop if an error at the start of the loop is detected.

--- include/parserlib/parse_algorithms.hpp
+++ include/parserlib/parse_algorithms.hpp
@@ -50,51 +50,56 @@ namespace parserlib {
     }
 
 
-    //helper function for parsing optionals;
-    //if the function adds errors to the parse context,
-    //then the parsing of the optional is considered as failed,
-    //even if the function itself returns true.
-    template <class ParseContext, class F>
-    bool parse_optional_helper(ParseContext& pc, const F& fn) {
-        //store the current error state
-        const auto initial_error_state = pc.get_error_state();
-
-        //invoke the function
-        const bool result = fn();
-
-        //if errors are found, consider the parsing as failed
-        if (pc.get_errors().size() > initial_error_state.get_error_count()) {
-            pc.set_error_state(initial_error_state);
-            return false;
-        }
-
-        //no errors found, return the normal result
-        return result;
-    }
-
-
     /**
      * Invokes the given function.
+     * If new errors are detected, they are canceled.
      * @param pc the parse context.
      * @param fn the parse function.
      * @return always true.
      */
     template <class ParseContext, class F>
     bool parse_optional(ParseContext& pc, const F& fn) {
-        parse_optional_helper(pc, fn);
+        //store the current error state
+        const auto initial_error_state = pc.get_error_state();
+
+        //invoke the function; discard the result as it is not useful, optionals always return true
+        fn();
+
+        //if errors are found, consider the parsing as failed and cancel the errors
+        if (pc.get_errors().size() > initial_error_state.get_error_count()) {
+            pc.set_error_state(initial_error_state);
+        }
+
         return true;
     }
 
 
     /**
-     * Invokes the given function repeatedly, until it fails to parse.
+     * Invokes the given function repeatedly, until it fails to parse or an error is detected
+     * that starts at the parse position before the loop.
      * @param pc the parse context.
      * @param fn the parse function.
      * @return always true.
      */
     template <class ParseContext, class F>
     bool parse_loop_0(ParseContext& pc, const F& fn) {
-        while (parse_optional_helper(pc, fn)) {
+        while (true) {
+            const auto base_iterator = pc.get_iterator();
+            const auto base_error_state = pc.get_error_state();
+
+            //parse
+            const bool result = fn();
+
+            //if failed, stop the loop
+            if (!result) {
+                break;
+            }
+
+            //if new errors are detected and the first error starts at the base iterator, cancel the errors and stop the loop
+            if (pc.get_errors().size() > base_error_state.get_error_count() && pc.get_errors()[base_error_state.get_error_count()].begin() == base_iterator) {
+                pc.set_error_state(base_error_state);
+                break;
+            }
         }
         return true;
     }
